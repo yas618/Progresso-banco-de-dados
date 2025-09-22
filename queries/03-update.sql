bc5364e90073aba83696657e1c29fe2ccc71f0c9
Yasmin Porto <yasmin.porto@aluno.senai.br>
2025-09-22 09:42:24 -0300

    finalização

---
+++ queries/03-update.sql
@@ -0,0 +1,13 @@
+UPDATE  usuarios SET username ='Progamer2025'
+ WHERE username "NobMaster";
+
+ SELECT * FROM usuarios;
+
+UPDATE usuarios SET pontos = pontos + 1000;
+
+SELECT * FROM usuarios;
+
+UPDATE usuarios SET email = 'master@gmail'     
+WHERE email =
+ 'master@email';           
+SELECT * FROM usuarios;                                             
---
+++ queries/04-deletes.sql
@@ -0,0 +1,11 @@
+DELETE * FROM usuarios
+    WHERE username = 'CheatCodeKing';
+
+SELECT * FROM usuarios
+WHERE id= 13;
+
+SELECT * FROM usuarios
+WHERE id =13;
+DELETE FROM usuarios
+    WHERE pontos <  2000;
+    SELECT * FROM   usuarios;
